9b5257e5b8d87c9a8a3d2b961ee41081ad6e558e
vic1er03 <azariaazaria473@gmail.com>
2025-03-25 10:23:49 +0100

    Update Das.py

--- Das.py
+++ Das.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
-url = "Challenge dataset traité.xlsx"
-data = pd.read_excel(url)
+url = "Challenge_dataset_traité.csv"
+data = pd.read_csv(url)
 st.dataframe(data)
 
